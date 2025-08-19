1a17ae71e61cc6a5a99db69d4b16361e262a54f6
Bart Feenstra <bart@mynameisbart.com>
2025-08-19 10:24:57 +0100

    Upgrade to Betty 0.5.0a12

--- betty_nginx/__init__.py
+++ betty_nginx/__init__.py
@@ -3,24 +3,63 @@
 from pathlib import Path
 from typing import final
 
-from betty.event_dispatcher import EventHandlerRegistry
-from betty.locale.localizable import static, _, Localizable
+from betty.job import Job
+from betty.job.scheduler import Scheduler
+from betty.locale.localizable import _, Localizable, Plain
 from betty.machine_name import MachineName
+from betty.project import ProjectContext
 from betty.project.extension import ConfigurableExtension
-from betty.project.generate import GenerateSiteEvent
+from betty.project.generate import Generator
 from typing_extensions import override
 
 from betty_nginx.artifact import generate_configuration_file, generate_dockerfile_file
 from betty_nginx.config import NginxConfiguration
 
 
-async def _generate_configuration_files(event: GenerateSiteEvent) -> None:
-    await generate_configuration_file(event.project)
-    await generate_dockerfile_file(event.project)
+@final
+class GenerateConfigurationFile(Job[ProjectContext]):
+    """
+    Generate nginx.conf.
+    """
+
+    def __init__(self):
+        super().__init__(self.id_for(), priority=True)
+
+    @classmethod
+    def id_for(cls) -> str:
+        """
+        Get the job ID.
+        """
+        return "betty-nginx-generate-configuration-file"
+
+    @override
+    async def do(self, scheduler: Scheduler[ProjectContext], /) -> None:
+        await generate_configuration_file(scheduler.context.project)
 
 
 @final
-class Nginx(ConfigurableExtension[NginxConfiguration]):
+class GenerateDockerfile(Job[ProjectContext]):
+    """
+    Generate Dockerfile.
+    """
+
+    def __init__(self):
+        super().__init__(self.id_for(), priority=True)
+
+    @classmethod
+    def id_for(cls) -> str:
+        """
+        Get the job ID.
+        """
+        return "betty-nginx-generate-dockerfile"
+
+    @override
+    async def do(self, scheduler: Scheduler[ProjectContext], /) -> None:
+        await generate_dockerfile_file(scheduler.context.project)
+
+
+@final
+class Nginx(Generator, ConfigurableExtension[NginxConfiguration]):
     """
     Integrate Betty with nginx (and Docker).
     """
@@ -33,7 +72,7 @@ class Nginx(ConfigurableExtension[NginxConfiguration]):
     @override
     @classmethod
     def plugin_label(cls) -> Localizable:
-        return static("Nginx")
+        return Plain("Nginx")
 
     @override
     @classmethod
@@ -43,8 +82,8 @@ class Nginx(ConfigurableExtension[NginxConfiguration]):
         )
 
     @override
-    def register_event_handlers(self, registry: EventHandlerRegistry) -> None:
-        registry.add_handler(GenerateSiteEvent, _generate_configuration_files)
+    async def generate(self, scheduler: Scheduler[ProjectContext]) -> None:
+        await scheduler.add(GenerateConfigurationFile(), GenerateDockerfile())
 
     @override
     @classmethod
